f44a8983fa0a9dae514c3f6d20e4ca580a73727c
Kamran0925 <kamranrizwan0925@gmail.com>
2025-06-24 17:42:59 +0500

    Add styles fixes for robofriends landing page

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,19 +1,39 @@
+import React from "react";
+import Card from "./Card";
 
-import React from 'react'
-import Card from './Card';
 class CardList extends React.Component {
-  
-  render(){
+  render() {
+    if (this.props.robots.length === 0) {
+      return (
+        <div className="loading-container">
+          <p
+            style={{
+              marginTop: "30px",
+              fontSize: "22px",
+              lineHeight: "2rem",
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            Oops! No robots found with this name. <br /> Please try another.
+          </p>
+        </div>
+      );
+    }
+
     return (
       <div>
-        {this.props.robots.map((robot, i) => 
-          <Card key={i} id={robot.id} name={robot.name} email={robot.email}></Card>
-        )}
+        {this.props.robots.map((robot, i) => (
+          <Card
+            key={i}
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+          ></Card>
+        ))}
       </div>
     );
   }
-  
-  
 }
 
 export default CardList;
